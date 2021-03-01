8d7f03c7c2a59b9157a48c85665c8eafe9df68ed
BenDahGreat <78873879+BenDahGreat@users.noreply.github.com>
2021-03-01 10:44:06 -0600

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def main():
-    return " your mum"
+    return "if this message appears, its probably working"
 
 def run():
     app.run(host="0.0.0.0", port=8080)
